08dbf9426406550c428493618f88e5baa6fae22a
liuliguo <liulg@yxqiche.com>
2020-03-02 12:28:14 +0800

    11111

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * 修改11111
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
